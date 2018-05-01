3da17b2f63937f0c402bdda6f3bf1aa36ff3db5b
Ray <mkeller1991@gmail.com>
2018-05-01 08:39:15 -0500

    update

---
+++ APM/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
+
+@NgModule({
+  declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe ],
+  imports: [ BrowserModule, FormsModule ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
