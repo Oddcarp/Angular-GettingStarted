d6f68739144ac4804f6c0ebf96f7e0dc356b5e3d
jisamk1 <McKenzie.Keller@Jbhunt.com>
2018-05-29 12:21:22 -0500

    additional routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,12 +4,36 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductGuardService } from './products/product-guard.service';
 
 
 @NgModule({
-  declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe ],
-  imports: [ BrowserModule, FormsModule ],
-  providers: [],
+  declarations: [ 
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe, 
+    StarComponent, 
+    ProductDetailComponent, 
+    WelcomeComponent ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule, 
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id',
+        canActivate:[ProductGuardService],
+         component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ], {useHash: false}) ],
+  providers: [ProductGuardService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
